2f7e8d03f369ec993fe68725b6d51ff16e9983c5
Abhijeet Gawali <gawalishambu@gmail.com>
2026-08-07 18:01:21 +0530

    Add WhyChooseUs component and StickyScroll functionality; update HeroSection styles

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -16,7 +16,7 @@ function HeroSection() {
 <>
     <Spotlight/>
     <div
-    className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0"
+    className="h-screen md:h-160 w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0"
     >
       <div className="p-4 relative z-10 w-full text-center">
             <h1
